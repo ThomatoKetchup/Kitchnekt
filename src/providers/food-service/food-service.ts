135260e0a56ac09c464fb44e667c559a8e02faf6
didicluber <dylanouanounou@yahoo.fr>
2019-03-08 15:02:42 +0100

    change logo

--- src/providers/food-service/food-service.ts
+++ src/providers/food-service/food-service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
 
 /*
   Generated class for the FoodServiceProvider provider.
