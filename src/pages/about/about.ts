f9cb5762bc4767d7e5b241dbb014beb521256788
Alex <alex.hourcade@hotmail.fr>
2019-04-28 19:40:13 +0200

    ajustement et ajout de la partie recette

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {RecettePage} from "../recette/recette";
 
 @Component({
   selector: 'page-about',
@@ -9,6 +10,10 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  pushRecette(){
+    this.navCtrl.push(RecettePage)
   }
 
 }
